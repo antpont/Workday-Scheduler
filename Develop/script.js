41b27ffd4bb69748b0f01a517f5b6ba51ba7223d
Anthony Pontecorvo <antpont80@gmail.com>
2023-01-31 18:13:33 -0500

    Added comments and to Readme file

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
 
 var dayjs = dayjs()
-
+//Fromatting and showing the current date using dayjs
 $('#currentDay').text(dayjs.format("dddd, MM DD, YYYY"))
 
 var currentHour = dayjs.hour()
@@ -29,7 +9,7 @@ var timeBlock = $('.description')
 
 timeBlock.each(function () {
   var id = $(this).attr('id')
-
+//Setting the blocks to be able to tell if it is in the past, present, or future
   if (currentHour > id) {
     $(this).addClass('past')
   } else if (currentHour == id) {
@@ -41,7 +21,7 @@ timeBlock.each(function () {
     $(this).addClass('future')
   }
 })
-
+//When you click the button your event will show and save to local storage
 $('.btn').on("click", function() {
   var key = $(this).attr('id')
   var task = $(this).siblings('textarea').val()
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Workday-Scheduler
 To help manage your time and schedule you can add events into this planner.
+When you click the button it will save your events scheduled for that hour and it is color coded for the past, present, and future times.
